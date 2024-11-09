c9a95e1879cab539ef1ac9541bbd0092297a48ef
Fernanda <fernanda19@usf.edu>
2024-11-08 19:56:44 -0500

    Create asset module with asset list and getAssetById function.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Coding-Project-15
+Financial Portfolio Management System that allows users to track and analyze investments. Created using ES6 modules 
---
+++ asset.js
@@ -0,0 +1,17 @@
+// create asset module 
+// named exports: 
+export const assets = [
+    {id: 1, name: 'asset a', type: 'stock', price: 30, quantity: 200},
+    {id: 2, name: 'asset b', type: 'bond', price: 1000, quantity: 20},
+    {id: 3, name: 'asset c', type: 'stock', price: 40, quantity: 300},
+];
+// create function to find asset by id 
+export function getAssetById(id){
+    const asset = assets.find(a => a.id === id);
+    // if function to return details if asset found and if not log an error message 
+    if (asset) { 
+        return `${asset.name}, it is a ${asset.stock} priced at ${asset.price} and the quantity is ${asset.quantity}`
+    } else {
+        console.log("error: asset could not be found");
+    }
+};
