f20b8c9050d374cbe9bed20fffc37bf9b79782a9
Fernanda <fernanda19@usf.edu>
2024-11-10 17:48:56 -0500

    CSS file

--- main.js
+++ main.js
@@ -11,14 +11,29 @@ console.log(Transaction);
 const portfolioValue = calculatePortfolioValue (assets);
 console.log(`Portfolio value is ${portfolioValue}`); //displays total portfolio value
 
+//to show on webpage:
+function portfolioOnScreen (value) {
+    const portfolioV = document.getElementById("portfolio-value");
+    portfolioV.textContent = `${value.toLocaleString()}`; 
+}
+
 function displayPortfolio (assets) {
     const portfolioAllocation = getPortfolioAllocation (assets);
+    const allocationList = document.getElementById("allocation-list")
+    allocationList.innerHTML =""; //this is to show output on screen
+
     portfolioAllocation.forEach(asset => {
         console.log(`Asset ${asset.name} represents ${asset.assetPercentage}% of the portfolio`)
-    }); //displays allocation of each asset 
-}
+    const p = document.createElement("p");
+    p.textContent = `${asset.name} represents ${asset.assetPercentage}% of the portfolio`
+    allocationList.appendChild(p) })}; //displays allocation of each asset 
+
+    
 console.log("Initial portfolio allocation:");
+
 displayPortfolio(assets)
+
+portfolioOnScreen(portfolioValue)
 // create new transactions using the default class
 const newtransaction1 = new Transaction (1, "sell", 3 ); 
 console.log(`Sell 3 units of asset ID 1`);
@@ -28,6 +43,7 @@ console.log(`Buy 15 units of asset ID 2`);
 
 
 let updatedPortfolio = calculatePortfolioValue(assets);
+portfolioOnScreen(updatedPortfolio)
 console.log(`Updated portfolio value after the transactions is $${updatedPortfolio}`)
 displayPortfolio(assets); 
 
